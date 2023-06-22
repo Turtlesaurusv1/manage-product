c6ee919092a26870434a53041b82a9ce5612bb1f
ngnshud@gmail.com <ngnshud@gmail.com>
2023-06-23 00:14:49 +0700

    index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-const path = require('path');
 
-// This will give you the absolute path of the current working directory
-const currentPath = path.resolve();
+  //1.input จะต้องมีความยาวมากกว่าหรือเท่ากับ 6 ตัวอักษร เช่น
+  function validatePinCode1(pinCode) {
+    const regex = /^\d{6,}$/;
+  
+    if (regex.test(pinCode)) {
+      return true;
+    }
+    return false
+  }
+  console.log(validatePinCode1('172')); // false
+  console.log(validatePinCode1('1725465')); // true
 
-// This will join two paths together to create an absolute path
-const myPath = path.join(__dirname, 'database', 'entities', );
 
-console.log(currentPath); // Output: C:\Users\YourUser\MyProject
-console.log(myPath); // Output: C:\Users\YourUser\MyProject\myFolder\myFile.js
+  //2. input จะต้องกันไม่ให้มีเลขซ้ำติดกันเกิน 2 ตัว
+  function validatePinCode2(pinCode) {
+    const consecutiveRegex = /(.)\1{2}/;
+  
+    if (consecutiveRegex.test(pinCode)) {
+      return false;
+    }
+  
+    return true;
+  }
+  console.log(validatePinCode2('111822')); // false
+  console.log(validatePinCode2('1112762')); // true
